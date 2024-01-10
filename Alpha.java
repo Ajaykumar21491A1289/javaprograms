4c77a1c8f0ff65a6746972fe223701b1f1207327
Ajaykumar21491A1289 <ajju1677143@gmail.com>
2024-01-10 21:09:45 +0530

    first commit

---
+++ Alpha.java
@@ -0,0 +1,42 @@
+
+public class Print
+{
+    print(char a[])
+    {
+        int i;
+        for(i=0;i<50;i++)
+        {
+            System.out.print("%c",arr[i]);
+        }
+    }
+}
+public class Alpha  
+    {    
+    public static void main(String args[])   
+    {    
+    //i for rows and j for columns      
+    //row denotes the number of rows you want to print  
+    int i, j, row = 100;   
+   char a[50]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};    
+    //Outer loop work for rows  
+    for (i=0; i<row; i++)   
+    {  
+    //inner loop work for space      
+    for (j=row-i; j>1; j--)   
+    {  
+    //prints space between two stars  
+      
+    }   
+    //inner loop for columns  
+    for (j=0; j<=i; j++ )   
+    {   
+    //prints star      
+    Print s=new Print(a[50]);
+    }   
+    //throws the cursor in a new line after printing each line  
+    System.out.println();   
+    }   
+    }   
+    } 
+    
+}
